2b50b74ae347e8e700c93b52e1ceb271078461ce
Aleksandra Salak <aleksandrasalak@gmail.com>
2019-12-07 20:36:02 +0100

    Changes in logic
    Slider of items

--- src/App.js
+++ src/App.js
@@ -9,129 +9,79 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      isActive: false,
-      name: null,
-      lastScroll: 0,
-      currentId: 0,
-      index: 0
+      nav1: null,
+      nav2: null
     };
-
-    this.handleShow = this.handleShow.bind(this);
-    this.handleData = this.handleData.bind(this);
-    this.handleScroll = this.handleScroll.bind(this);
   }
-
-  handleData = (value, name) => {
-    this.setState({ isActive: value, name: name });
-  };
 
   componentDidMount() {
-    window.addEventListener("scroll", this.handleScroll);
-  }
+    this.setState({
+      nav1: this.slider1,
+      nav2: this.slider2
+    });
 
-  componentWillUnmount() {
-    window.removeEventListener("scroll", this.handleScroll);
-  }
+    let slickListDiv = document.getElementsByClassName("cards-list")[0];
 
-  handleScroll(e) {
-    var element = document.getElementsByClassName("cards-list")[0];
+    slickListDiv.addEventListener("wheel", event => {
+      event.preventDefault();
 
-    if (this.state.lastScroll == undefined) {
-      this.state = {
-        lastScroll: element.scrollTop
-      };
-    } else if (element.scrollTop > this.state.lastScroll) {
-      // downscroll rules will be here
-      this.state = {
-        lastScroll: element.scrollTop
-      };
-      /*   this.setState(prevState => {
-        return {currentId: prevState.currentId + 1}
-     })
-      this.scrollTo(this.state.currentId + 1);*/
-      // console.log("dfsf");
-      element.scrollTo(0, this.myRef);
-      console.log(this.myRef);
-    } else if (element.scrollTop < this.state.lastScroll) {
-      this.state = {
-        lastScroll: element.scrollTop
-      };
-
-      /*   this.setState(prevState => {
-        return {currentId: prevState.currentId - 1}
-     })
-      console.log("fdfdf");
-      this.scrollTo(this.state.currentId - 1);*/
-      this.handleShow(10);
-    }
-  }
-
-  handleShow(i) {
-    this.setState({ index: i });
-    // console.log(i);
-    // console.log(this.refs[i]);
-    // this.refs[i].scrollIntoView({ block: "end", behavior: "smooth" });
-  }
-
-  scrollTo(id) {
-    this.itemRefs[id].scrollIntoView();
+      if (event.deltaY > 0) {
+        this.slider2.slickNext();
+        this.slider1.slickNext();
+      } else {
+        this.slider2.slickPrev();
+        this.slider1.slickPrev();
+      }
+    });
   }
 
   render() {
     var settings = {
-      dots: true,
-      appendDots: dots => (
-        <div>
-          <ul className="cards-list" onScroll={this.handleScroll}>
-            {" "}
-            {dots}
-          </ul>
-        </div>
-      ),
-      customPaging: (
-        i //i moze byc inne niz mysle!!! sprawdzic!!!
-      ) => (
-        <li key={i} ref={i => (this.myRef = i)}>
-          {Cards[i].Name + i}
-        </li>
-      ),
       infinite: false,
-      slidesToShow: 1,
       slidesToSrcoll: 1,
-      speed: 1200,
       vertical: true,
       arrows: false,
+      dots: false,
       verticalSwiping: true
     };
 
     return (
-      /* <div className="professy-interface">
-        <h1 className="professy-interface__header">Pick professy</h1>
-        {Cards.map((item, key) => (
-          <Card name={item.Name} key={key} parentCallback={this.handleData} />
-        ))}
-        {this.state.isActive ? (
-          <CardExtended
-            name={this.state.name}
-            parentCallback={this.handleData}
-          />
-        ) : null}
-      </div>
-      
-       <CardList active="Łowca nagród" />
-      */
-      /* <div class="app">
-        <Carousel auto axis={"y"} widgets={[CardList]}>
+      <div>
+        <Slider
+          asNavFor={this.state.nav2}
+          ref={slider => (this.slider1 = slider)}
+          slidesToShow={1}
+          fade={true}
+          className="profession-list"
+          swipe={false}
+          {...settings}
+        >
           {Cards.map((item, key) => (
             <Card name={item.Name} key={key} />
           ))}
-        </Carousel>
-      </div>*/
-      <Slider {...settings}>
-        {Cards.map((item, key) => (
-          <Card name={item.Name} key={key} />
-        ))}
-      </Slider>
+        </Slider>
+        <Slider
+          asNavFor={this.state.nav1}
+          ref={slider => (this.slider2 = slider)}
+          slidesToShow={3}
+          focusOnSelect={true}
+          className="cards-list"
+          centerMode={true}
+          {...settings}
+        >
+          {Cards.map((item, key) => (
+            <li
+              className={
+                item.Name == this.state.active
+                  ? "cards-list__item cards-list__item--active"
+                  : "cards-list__item"
+              }
+            >
+              {item.Name}
+            </li>
+          ))}
+        </Slider>
+      </div>
     );
   }
 }
